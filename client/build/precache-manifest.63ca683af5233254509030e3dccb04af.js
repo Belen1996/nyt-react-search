9ef548cc48fa8a4453cf5ab5d26c252c4f0b34ad
Mariano Bringas <mariano.bringas@activenetwork.com>
2018-11-04 21:34:34 -0600

    Fixed Heroku's API

--- client/build/precache-manifest.63ca683af5233254509030e3dccb04af.js
+++ client/build/precache-manifest.63ca683af5233254509030e3dccb04af.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "441b6d47773966cde260",
+    "revision": "17a89a6c4c2c441a25a0",
     "url": "/static/css/main.82941397.chunk.css"
   },
   {
-    "revision": "441b6d47773966cde260",
-    "url": "/static/js/main.441b6d47.chunk.js"
+    "revision": "17a89a6c4c2c441a25a0",
+    "url": "/static/js/main.17a89a6c.chunk.js"
   },
   {
     "revision": "13e1aea5818a8dfd00dd",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "80283780dbf05d1f67f7b661c9fed7c5",
+    "revision": "bbba9362b3b3ff139997e4381d87fc7e",
     "url": "/index.html"
   }
 ];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7c55d75b8b99c87cb4f98fe1d018e119.js"
+  "/precache-manifest.63ca683af5233254509030e3dccb04af.js"
 );
 
 workbox.clientsClaim();
